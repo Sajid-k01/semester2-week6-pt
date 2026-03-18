716a28e39ed044dadbe018182f327db0c15a95e1
SuperSaj01 <saj.kha01@gmail.com>
2026-03-18 01:06:41 +0000

    done

--- worksheet/worksheet.c
+++ worksheet/worksheet.c
@@ -19,6 +19,7 @@
  */
 
 #include "acutest.h"
+#include "stdio.h"
 
 /* ============================================================
  * FUNCTION PROTOTYPES - Do not modify these
@@ -49,7 +50,8 @@ int find_max(int *arr, int n, int *index);
  */
 int add_values(int a, int b) {
   // TODO: Implement this function
-  return 0; // Replace this
+  int sum = a + b;
+  return sum; // Replace this
 }
 
 /**
@@ -69,6 +71,9 @@ int add_values(int a, int b) {
  */
 void swap_values(int *a, int *b) {
   // TODO: Implement this function
+  int temp = *a;
+  *a = *b;
+  *b = temp;
   // Hint: You'll need a temporary variable
 }
 
@@ -86,7 +91,12 @@ void swap_values(int *a, int *b) {
  */
 int sum_array(int *arr, int n) {
   // TODO: Implement this function
-  return 0; // Replace this
+  int sum = 0;
+  for(int i = 0; i < n; i++)
+  {
+   sum += *(arr + i);
+  }
+  return sum; // Replace this
 }
 
 /**
@@ -107,6 +117,12 @@ int sum_array(int *arr, int n) {
  */
 void reverse_array(int *arr, int n) {
   // TODO: Implement this function
+  for(int i = 0; i < n / 2; i++)
+  {
+    int temp = *(arr + i);
+    *(arr + i) = *(arr + n - 1 - i);
+    *(arr + n - 1- i) = temp;
+  }
 }
 
 /**
@@ -125,7 +141,14 @@ void reverse_array(int *arr, int n) {
 double average(int *arr, int n) {
   // TODO: Implement this function
   // Hint: Be careful with integer division!
-  return 0.0; // Replace this
+  double sum = 0;
+  for(int i = 0; i < n; i++)
+  {
+   sum += *(arr + i);
+  }
+
+  double avg = sum / n;
+  return avg; // Replace this
 }
 
 /**
@@ -149,7 +172,16 @@ double average(int *arr, int n) {
 int find_max(int *arr, int n, int *index) {
   // TODO: Implement this function
   *index = 0;
-  return 0; // Replace this
+  int max = *arr;
+  for(int i=0; i<n; i++)
+  {
+    if(max < *(arr + i))
+    {
+      *index = i;
+      max = *(arr + i);
+    }
+  }
+  return max; // Replace this
 }
 
 /* ============================================================
